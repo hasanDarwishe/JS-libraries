d44b20bfc0127b5ca2ac0a5343f0471f6ba2421e
hasanDarwishe <99066758+hasanDarwishe@users.noreply.github.com>
2022-02-06 21:21:23 +0200

    Update drawLib.js

--- drawLib.js
+++ drawLib.js
@@ -1,1 +1,21 @@
-
+class SVG {
+constructor (){}
+  setLocation (location){
+    var loc = location;
+   }
+  setWidth (width=100,unit="px"){
+  var Width= width;
+  var w= width + unit;
+  }
+  setHeight(height=100,unit="px"){
+  var Height=height;
+  var h = height+unit;
+  }
+  setStyle (style="icon", border= "2px solid black;", backgroundColor="white"){
+    var Style= "border:" + border ;
+  if(style != "rounded"){
+    Style += "border-radius:0px;";
+   }
+  else if(style== "rounded"){Style += "border-radius:50px;";}
+  }
+}
